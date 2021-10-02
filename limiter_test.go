c4c90450fa8b923638164204751b273d3222dd66
Narasimha1997 <yogaimpression@gmail.com>
2021-10-02 22:05:52 +0530

    Added a test for SyncLimiter

--- limiter_test.go
+++ limiter_test.go
@@ -176,3 +176,95 @@ func TestLimiterCleanup(t *testing.T) {
 		t.Fatalf("Calling ShouldAllow() on inactive limiter did not throw any errors.")
 	}
 }
+
+func TestConcurrentSyncLimiter(t *testing.T) {
+	nRuns := 10
+	duration := time.Second * 1
+
+	// 100 tasks must be allowed to execute
+	// for every `duration` interval.
+	var limit uint64 = 100
+
+	// create a limiter, that is shared across go routines:
+	sharedLimiter := NewSyncLimiter(limit, duration)
+	defer sharedLimiter.Kill()
+
+	// launch N go-routines:
+	nRoutines := 4
+
+	// dry run, this will allow rate-limiter to stabilize:
+	isDry := true
+
+	// test with accuracy +/- 3, modify this variable to
+	// test accuracy for various error offsets, 0 is the most
+	// ideal case.
+	var allowanceRange uint64 = 20
+
+	counterSlice := make([]uint64, nRoutines)
+
+	routine := func(idx int, wg *sync.WaitGroup) {
+
+		defer wg.Done()
+
+		// no need of mutex locking the counterSlice
+		// because each goroutine has access to only a
+		// unique index `idx` of the slice.
+		counterSlice[idx] = 0
+		j := 0
+
+		// Use of time.Tick in production is discouraged.
+		// time.Tick cannot be stopped, we are using it because
+		// this is a test code.
+		for range time.Tick(2 * time.Millisecond) {
+			canAllow, err := sharedLimiter.ShouldAllow(1)
+			if err != nil {
+				break
+			}
+
+			if canAllow {
+				counterSlice[idx]++
+			}
+
+			j++
+			if j%500 == 0 {
+				break
+			}
+		}
+	}
+
+	for i := 0; i < nRuns; i++ {
+		// create a wait group and
+		wg := sync.WaitGroup{}
+		for j := 0; j < nRoutines; j++ {
+			wg.Add(1)
+			go routine(j, &wg)
+		}
+
+		wg.Wait()
+
+		// sum over the counterSlice and check accuracy:
+		var count uint64 = 0
+		for _, partialCount := range counterSlice {
+			count += partialCount
+		}
+
+		// check accuracy of counter
+		if !isDry {
+			if (limit-allowanceRange) <= count && count <= (limit+allowanceRange) {
+				fmt.Printf(
+					"Iteration %d, Allowed tasks: %d, passed rate limiting accuracy test.\n",
+					(i+1)-2, count,
+				)
+			} else {
+				t.Fatalf(
+					"Accuracy test failed, expected results to be in +/- %d error range, but got %d",
+					allowanceRange, count,
+				)
+			}
+		}
+
+		if i == 1 {
+			isDry = false
+		}
+	}
+}
